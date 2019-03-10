279c87e43a175dcc2103dc978851dcedde32796a
sbx_user1059 <sbx_user1059@ip-10-128-167-249.eu-west-2.compute.internal>
2019-03-10 13:02:55 +0000

    Sun Mar 10 13:02:55 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -27,21 +27,39 @@ public class CheckoutSolution {
 
     public Integer checkout(String skus) {
 
+        try {
+            Map<String, Integer> itemsInBasket = calculateItemsRequested(skus);
+
+            //Do multi pricing bit here, will keep simple for now
+            //Do normal pricing first
+            itemsInBasket.entrySet().for
+        } catch(IllegalArgumentException e) {
+            //Return -1 for unknown skus
+            return -1;
+        }
+
+
+    }
+
+    private  Map<String, Integer> calculateItemsRequested(String skus) {
         Map<String, Integer> itemTracker = new HashMap<>();
         String[] skuArray = skus.split(",");
         for (String sku : skuArray) {
             if(!pricingTable.containsKey(sku)) {
-                return -1;
+                throw new IllegalArgumentException(String.format("Unexpected sku %s", sku));
             }
 
             if(itemTracker.containsKey(sku)) {
                 Integer currentCount = itemTracker.get(sku);
-                itemTracker.
+                itemTracker.put(sku, currentCount + 1);
+            } else {
+                itemTracker.put(sku, 1);
             }
         }
-
+        return  itemTracker;
     }
 }
+
 
 
 
