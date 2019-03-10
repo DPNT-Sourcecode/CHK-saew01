864cd7b8044be7080409c214a7f70b18b00030f2
sbx_user1059 <sbx_user1059@ip-10-128-167-249.eu-west-2.compute.internal>
2019-03-10 14:12:43 +0000

    Sun Mar 10 14:12:43 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -73,17 +73,16 @@ public class CheckoutSolution {
                 Integer requestedItems = itemInBasket.getValue();
                 List<Multibuy> multibuysToCheck = itemInfo.getMultibuys();
 
-                for(Multibuy multibuy : multibuysToCheck) {
-
-                }
+                
             }
         }
     }
 
-    private Optional<PriceReductionMultibuy> getBestApplicablePriceMultibuy(List<Multibuy> multibuyList) {
+    private Optional<PriceReductionMultibuy> getBestApplicablePriceMultibuy(List<Multibuy> multibuyList, Integer requestedNumber) {
         Optional<PriceReductionMultibuy> bestMultibuy = Optional.empty();
         for (Multibuy multibuy : multibuyList) {
-            if(!bestMultibuy.isPresent() || bestMultibuy.get().getCount() < multibuy.getCount()) {
+            if( multibuy.getCount() >= requestedNumber && (!bestMultibuy.isPresent()
+                    || bestMultibuy.get().getCount() < multibuy.getCount())) {
                 bestMultibuy = Optional.of((PriceReductionMultibuy) multibuy);
             }
         }
@@ -113,5 +112,6 @@ public class CheckoutSolution {
         return  itemTracker;
     }
 }
+
 
 
