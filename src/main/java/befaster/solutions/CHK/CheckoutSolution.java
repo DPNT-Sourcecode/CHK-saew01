76afa5fdd31104d7138180a626f09e48817153fa
sbx_user1059 <sbx_user1059@ip-10-128-167-249.eu-west-2.compute.internal>
2019-03-10 12:50:55 +0000

    Sun Mar 10 12:50:55 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,8 +2,32 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CheckoutSolution {
+
+   private final Map<String, Item> pricingTable = new HashMap<>();
+
+    public CheckoutSolution() {
+        List<Item> items = createItems();
+        items.stream().map
+        pricingTable.
+    }
+
+    private List<Item> createItems() {
+        List<Item> itemList = new ArrayList<>();
+        itemList.add(new Item("A", 50));
+        itemList.add(new Item("B", 50));
+        itemList.add(new Item("C", 50));
+        itemList.add(new Item("D", 50));
+        return itemList;
+    }
+
     public Integer checkout(String skus) {
         throw new SolutionNotImplementedException();
     }
 }
+
--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,5 +1,6 @@
 package befaster.solutions.CHK;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,6 +14,20 @@ public class CheckoutSolutionTest {
 
     @Test
     public void checkout_shouldReturnNegativeOneOnIllegalValues() {
-        this.checkoutSolution.
+        Integer actual = this.checkoutSolution.checkout("This is in valid");
+        Assert.assertEquals("Expected -1 to be the result on illegal input", Integer.valueOf(-1), actual);
+    }
+
+    @Test
+    public void checkout_shouldCalculateBasketCorrectly() {
+        Integer actual = this.checkoutSolution.checkout("A,B,C");
+        Assert.assertEquals(Integer.valueOf(100), actual);
+    }
+
+    @Test
+    public void checkout_shouldCalculateMultibuyCorrectly() {
+        Integer actual = this.checkoutSolution.checkout("A,A,A");
+        Assert.assertEquals(Integer.valueOf(130), actual);
     }
 }
+
