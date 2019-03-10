8869947c46d9e32f1ef09cdc60f1914fbb1ac437
sbx_user1059 <sbx_user1059@ip-10-128-167-249.eu-west-2.compute.internal>
2019-03-10 12:38:55 +0000

    Sun Mar 10 12:38:55 UTC 2019

--- src/main/java/befaster/solutions/CHK/Item.java
+++ src/main/java/befaster/solutions/CHK/Item.java
@@ -1,4 +1,21 @@
 package befaster.solutions.CHK;
 
 public class Item {
+
+    private String sku;
+    private Integer price;
+
+    public Item(String sku, Integer price) {
+        this.sku = sku;
+        this.price = price;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
 }
+
--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,4 +1,19 @@
 package befaster.solutions.CHK;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class CheckoutSolutionTest {
+    private CheckoutSolution checkoutSolution;
+
+    @Before
+    public void setUp() {
+        checkoutSolution = new CheckoutSolution();
+    }
+
+    @Test
+    public void checkout_shouldReturnNegativeOneOnIllegalValues() {
+        this.checkoutSolution.
+    }
 }
+
