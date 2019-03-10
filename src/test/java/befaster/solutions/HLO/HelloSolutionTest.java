953c58c84bad0de0bb5bad319745ff78c9df2fe6
sbx_user1059 <sbx_user1059@ip-10-128-167-249.eu-west-2.compute.internal>
2019-03-10 12:25:22 +0000

    Sun Mar 10 12:25:22 UTC 2019

---
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -0,0 +1,12 @@
+package befaster.solutions.HLO;
+
+import org.junit.Test;
+
+public class HelloSolutionTest {
+
+    private HelloSolution = new 
+    @Test
+    public void hello_returnsExpected() {
+
+    }
+}
