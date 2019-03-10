89a379bb289cf30f18b206ede111cc9aa9270ce3
sbx_user1059 <sbx_user1059@ip-10-128-167-249.eu-west-2.compute.internal>
2019-03-10 12:19:19 +0000

    Sun Mar 10 12:19:19 UTC 2019

--- src/test/java/befaster/solutions/SUM/SumSolutionTest.java
+++ src/test/java/befaster/solutions/SUM/SumSolutionTest.java
@@ -11,23 +11,25 @@ public class SumSolutionTest {
 
     @Before
     public void setUp() {
-
         sum = new SumSolution();
     }
 
     @Test
     public void compute_correctlyAddsTwoValidValues() {
         assertThat(sum.compute(1, 1), equalTo(2));
+        assertThat(sum.compute(50, 100), equalTo(150));
+        assertThat(sum.compute(0,32), equalTo(32));
     }
 
-    @Test()
+    @Test(expected = IllegalArgumentException.class)
     public void compute_throwsIllegalArgumentExceptionIfNegative() {
         sum.compute(-1, 20);
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void compute_throwsIllegalArgumentExceptionIfOver100() {
-
+        sum.compute(1, 101);
     }
 }
+
 
