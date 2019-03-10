f56c7e88afb3bd8670418bd26c1f9abf76e0ffe1
sbx_user1059 <sbx_user1059@ip-10-128-167-249.eu-west-2.compute.internal>
2019-03-10 12:21:43 +0000

    Sun Mar 10 12:21:43 UTC 2019

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -5,7 +5,15 @@ import befaster.runner.SolutionNotImplementedException;
 public class SumSolution {
 
     public int compute(int x, int y) {
-        throw new SolutionNotImplementedException();
+        if(isValidValue(x) && isValidValue(y)) {
+            return x + y;
+        }
+        throw new IllegalArgumentException("Invalid parameters");
+    }
+
+    private boolean isValidValue(int value) {
+        return value >= 0 && value <= 100;
     }
 
 }
+
